acb6c76cd414df37513efea32480c1cea24e2cd6
Emmanuel1472 <emmanuelnater@gmail.com>
2023-04-26 01:29:18 +0100

    newone

--- ovs1/settings.py
+++ ovs1/settings.py
@@ -87,22 +87,23 @@ WSGI_APPLICATION = 'ovs1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 """
-
-
 
 import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.parse(env('DATABASE_URL'))
 }
+
+"""
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
